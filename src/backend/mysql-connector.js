e2662cc5c4520e1930fe3df782279303f4d1914a
Marcelo García <garcia.marcelor@gmail.com>
2022-08-15 22:04:11 -0300

    Otras opciones en clase

--- src/backend/mysql-connector.js
+++ src/backend/mysql-connector.js
@@ -3,16 +3,16 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : 'mysql-server',
+    host     : 'mysql-server',  //Si es local acá va localhost y se conecta la mySQL instalado en la maquina
     port     : '3306',
     user     : 'root',
     password : 'userpass',
-    database : 'smart_home'
+    database : 'smart_home'  //nombre de la base de datos
 });
 
 //=======[ Main module code ]==================================================
 
-connection.connect(function(err) {
+connection.connect(function(err) {      //test de que me puedo conectar, devuelve un callback que es el function(err)
     if (err) {
         console.error('Error while connect to DB: ' + err.stack);
         return;
@@ -20,6 +20,6 @@ connection.connect(function(err) {
     console.log('Connected to DB under thread ID: ' + connection.threadId);
 });
 
-module.exports = connection;
+module.exports = connection;        //exporta el módulo de codigo para que pueda ser utilizada en el mismo proyecto. Esto dsps nos permite ejecutar queries
 
 //=======[ End of file ]=======================================================
